340fbf579a115891f700344a2d39cf6129a4415f
Mike Dalton <mikedalton@github.com>
2020-05-24 10:21:09 +0900

    Remove default command, use !info-bot

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.channels.cache.get('516778810465583124').send(("Hi! I'm your friendly neighborhood NFC-Bot, here to lend a hand! Start out with !help. For any questions or feedback, hit up @Sm0keWag0n."));
+  //client.channels.cache.get('516778810465583124').send(("Hi! I'm your friendly neighborhood NFC-Bot, here to lend a hand! Start out with `!info-bot`. For any questions or feedback, hit up @Sm0keWag0n."));
 })
 
 client.on("message", msg => {
@@ -74,7 +74,7 @@ client.on("message", msg => {
                     user.send(dmEmbed);
                 }
             }
-        } else {
+        } else if (msg.content == "!info-bot") {
             msg.channel.send(commandListEmbed);
         }
     }
